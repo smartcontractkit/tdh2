db809f1eb90918c1cb247bc96d77fbedc0b0f88d
stchrysa <chrysa@smartcontract.com>
2023-05-24 21:40:22 +0200

    Removed key matterial from offchain config
    Signed-off-by: stchrysa <chrysa@smartcontract.com>

--- go/ocr2/decryptionplugin/config/config.go
+++ go/ocr2/decryptionplugin/config/config.go
@@ -1,10 +1,6 @@
 package config
 
 import (
-	"fmt"
-	"math"
-
-	"github.com/smartcontractkit/libocr/commontypes"
 	"google.golang.org/protobuf/proto"
 )
 
@@ -24,18 +20,4 @@ func DecodeReportingPluginConfig(raw []byte) (*ReportingPluginConfigWrapper, err
 
 func EncodeReportingPluginConfig(rpConfig *ReportingPluginConfigWrapper) ([]byte, error) {
 	return proto.Marshal(rpConfig.Config)
-}
-
-func EncodeOracleIdtoKeyShareIndex(oracleID commontypes.OracleID, keyShareIndex int) *OracleIDtoKeyShareIndex {
-	return &OracleIDtoKeyShareIndex{
-		OracleId:      uint32(oracleID),
-		KeyShareIndex: uint32(keyShareIndex),
-	}
-}
-
-func DecodeOracleIdtoKeyShareIndex(oracleIDtoKeyShareIndex *OracleIDtoKeyShareIndex) (commontypes.OracleID, int, error) {
-	if oracleIDtoKeyShareIndex.OracleId > math.MaxUint8 {
-		return 0, 0, fmt.Errorf("oracleID is larger than MAX_UINT8")
-	}
-	return commontypes.OracleID(oracleIDtoKeyShareIndex.OracleId), int(oracleIDtoKeyShareIndex.KeyShareIndex), nil
 }
