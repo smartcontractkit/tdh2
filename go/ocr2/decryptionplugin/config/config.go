d8bc79dc1d8309965c5367084ea928fe9bde4792
Bolek Kulbabinski <1416262+bolekk@users.noreply.github.com>
2023-06-07 17:55:32 -0700

    Customizable config parser
    DecryptionReportingPluginFactory takes an additional config parser that can extract Threshold-specific config from a potentially larger config struct.

--- go/ocr2/decryptionplugin/config/config.go
+++ go/ocr2/decryptionplugin/config/config.go
@@ -21,3 +21,15 @@ func DecodeReportingPluginConfig(raw []byte) (*ReportingPluginConfigWrapper, err
 func EncodeReportingPluginConfig(rpConfig *ReportingPluginConfigWrapper) ([]byte, error) {
 	return proto.Marshal(rpConfig.Config)
 }
+
+//go:generate mockery --quiet --name ConfigParser --output ./mocks/ --case=underscore
+type ConfigParser interface {
+	ParseConfig(offchainConfig []byte) (*ReportingPluginConfigWrapper, error)
+}
+
+type DefaultConfigParser struct {
+}
+
+func (p *DefaultConfigParser) ParseConfig(offchainConfig []byte) (*ReportingPluginConfigWrapper, error) {
+	return DecodeReportingPluginConfig(offchainConfig)
+}
